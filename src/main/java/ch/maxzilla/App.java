02454d325efec9955fe92903c8746e4587beea65
maxzilla222 <andreasfr@gmx.net>
2017-02-02 16:44:49 +0100

    Initial commit

---
+++ src/main/java/ch/maxzilla/App.java
@@ -0,0 +1,13 @@
+package ch.maxzilla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
